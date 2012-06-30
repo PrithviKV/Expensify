dc9011abb61b484469554649642887802f9f6724
Prithvi <kv.prithvi5@gmail.com>
2012-06-30 16:50:09 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expensify::Application.config.secret_token = 'f86a68b00491c9b0354c05ac446bc25ad95dc05ca30deeedb7a67bd560d502f45dcb10f2c4c2116cead54db449ec80727d5dea439b824884f0193723b1e2d75e'
