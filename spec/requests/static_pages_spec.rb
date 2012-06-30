af4428c558989e9318842b0cebff500a2b680432
Prithvi <kv.prithvi5@gmail.com>
2012-06-30 20:12:02 +0800

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+describe "Home page" do
+
+    it "should have the h1 'Expenses Tracker'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Expenses Tracker')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Expenses Tracker | Home")
+    end
+  end
+
+
+ describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Expenses Tracker | Help")
+    end
+  end
+
+
+describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Expenses Tracker | About Us")
+    end
+  end
+  
+
+
+
+end
